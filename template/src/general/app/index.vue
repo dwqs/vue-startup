9b85139679b8073048c93224a946c473288142a6
dwqs <461147874@qq.com>
2017-04-20 11:07:27 +0800

    Add general

---
+++ template/src/general/app/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <h3>{{title}}</h3>
+        <hello />
+        <router-link to="/social/info">查看项目信息</router-link>
+    </div>
+</template>
+
+<script>
+    import './index.css';
+    import Hello from '@components/hello/';
+
+    export default{
+        data () {
+            return {
+                title: 'vue-webpack'
+            };
+        },
+
+        components: {
+            Hello
+        }
+    };
+</script>
+
+<style></style>
