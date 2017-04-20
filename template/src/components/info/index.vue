a11a744bd595cbbd8d2aeeabf19c3d1d8b3ad9b3
dwqs <461147874@qq.com>
2017-04-20 11:06:40 +0800

    Add components

---
+++ template/src/components/info/index.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <div class="info">
+        <h2>{{info}}</h2>
+        <h3>Project Name:social</h3>
+        <h3>Project Version:0.0.1</h3>
+        <h3>Author:yuankang@followme.com</h3>
+        <h3>Desc: A new Vue.js project</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'info',
+    data () {
+        return {
+            info: 'project info:'
+        };
+    }
+};
+</script>
+
+<style></style>
