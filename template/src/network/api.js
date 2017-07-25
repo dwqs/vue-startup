1816814c5f2119bf91f083eedc91d889629517f7
dwqs <461147874@qq.com>
2017-07-25 15:03:08 +0800

    [misc]update api

--- template/src/network/api.js
+++ template/src/network/api.js
@@ -7,7 +7,7 @@ import network from './index';
 export default {
     async getIndex(){
         const res = await network.get({
-            url: `https://api.github.com/emojis`,
+            url: `https://api.github.com/users/dwqs/repos`,
             data: {}
         });
         return res;
