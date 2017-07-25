e8f11dcef9090b8f05883b2c23a26db61980cdec
dwqs <461147874@qq.com>
2017-07-25 14:03:58 +0800

    [misc]update app component

--- template/src/general/app/index.vue
+++ template/src/general/app/index.vue
@@ -7,11 +7,17 @@
             <a href="https://github.com/dwqs/vue-startup" target="_blank">vue-startup</a>
         </p>
         <router-link to="{{publicPath}}info">查看项目信息</router-link>
+        <div class="time">
+            <span> 当前时间: \{{new Date(curTime)}}</span>
+            <span @click="changeTime"> 点击更新当前时间</span>
+        </div>
     </div>
 </template>
 
 <script>
-    import './index.css';
+    import './index.less';
+    import {mapActions, mapGetters} from 'vuex';
+
     import Hello from '@components/hello/';
 
     export default{
@@ -19,6 +25,16 @@
             return {
                 title: 'vuejs 2 + webpack 2'
             };
+        },
+
+        computed: {
+            ...mapGetters({
+                curTime: 'getCurTime'
+            })
+        },
+
+        methods: {
+            ...mapActions(['changeTime'])
         },
 
         components: {
