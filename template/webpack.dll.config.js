2cc0eb95f4c8fca05fabf9d7bf8bb00f0be92fec
dwqs <461147874@qq.com>
2017-07-25 13:36:09 +0800

    [misc]add axios & error handle

--- template/webpack.dll.config.js
+++ template/webpack.dll.config.js
@@ -6,7 +6,8 @@ const webpack = require('webpack');
 module.exports = {
     entry: {
         vendor: [
-            'vue', 'vuex', 'vue-router', 'vuex-router-sync', 'babel-polyfill'
+            'vue', 'vuex', 'vue-router', 'vuex-router-sync', 'babel-polyfill',
+            'async-await-error-handling', 'axios'
         ]
     },
     output: {
