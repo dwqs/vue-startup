3fd1b7546f442bfe3557fb4fd2fe69c5887712cf
dwqs <461147874@qq.com>
2017-07-31 16:16:42 +0800

    update demo components

--- template/src/general/app/index.vue
+++ template/src/general/app/index.vue
@@ -16,14 +16,14 @@
 
 <script>
     import './index.less';
-    import {mapActions, mapGetters} from 'vuex';
-
     import Hello from '@components/hello/';
 
+    {{#if_eq state 'vuex'}}
+    import {mapActions, mapGetters} from 'vuex';
     export default{
         data () {
             return {
-                title: 'vuejs 2 + webpack 2'
+                title: 'vuejs 2 + webpack 3'
             };
         },
 
@@ -41,6 +41,23 @@
             Hello
         }
     };
+    {{/if_eq}}
+    {{#if_eq state 'mobx'}}
+    import {connect} from 'vue-mobx';
+    import timeModel from '@src/mobx/time';
+
+    const indexComponent = {
+        data () {
+            return {
+                title: 'vuejs 2 + webpack 3'
+            };
+        },
+        components: {
+            Hello
+        }
+    }
+    export default connect({timeModel})(indexComponent)
+    {{/if_eq}}
 </script>
 
 <style></style>
