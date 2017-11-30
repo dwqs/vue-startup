a71f24ea450adaba5d1907bf2f24617c6b261e21
dwqs <461147874@qq.com>
2017-11-30 15:36:26 +0800

    update

--- template/src/network/api.js
+++ template/src/network/api.js
@@ -5,7 +5,7 @@
 import network from './index';
 
 export default {
-    async getIndex(){
+    async getIndex () {
         const res = await network.get({
             url: `https://api.github.com/users/dwqs/repos`,
             data: {}
