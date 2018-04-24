80fb571bb2a9730dd5a6c180b8d1fe1371f6171c
dwqs <461147874@qq.com>
2018-04-24 15:59:15 +0800

    添加一个测试页面

---
+++ template/src/general/test/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="test">test page</div>
+</template>
+
+<script>
+    import './index.less';
+    import test from '@src/lib/test';
+    export default {
+        mounted () {
+            test();
+        }
+    }
+</script>
