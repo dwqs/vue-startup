6a4318450a9a19a3a5a1ca11651102e041c0bb8b
dwqs <461147874@qq.com>
2018-09-23 15:55:45 +0800

    update

--- template/src/general/info/index.vue
+++ template/src/general/info/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="info">
-    <h2>{{ info }}</h2>
+    <h2>\{{ info }}</h2>
     <h3>Project Name: vue-test</h3>
     <h3>Project Version: 0.0.1</h3>
     <h3>Author: dwqs</h3>
